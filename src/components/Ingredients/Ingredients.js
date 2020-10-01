89398b3b154232baab2219b15deef79b041f36b0
Abby Low <abby.low@plus65.com.sg>
2020-10-01 14:58:23 +0800

    remove redundant useeffect hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,22 +7,6 @@ import Search from './Search';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  useEffect(() => {
-    fetch('https://react-hook-practice-c5e6c.firebaseio.com/ingredients.json')
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          })
-        }
-        setUserIngredients(loadedIngredients); //the setState function created by useState hook won't be changed so no need to add as dependency
-      });
-  }, []);
-
   const addIngredientHandler = newIngredient => {
     // axios will automatically convert everything to JSON, but using fetch, we need to convert by ourselves
     fetch('https://react-hook-practice-c5e6c.firebaseio.com/ingredients.json', {
